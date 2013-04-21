6a920fc04dffe2652ed6849cb4a55c15119c34be
David Palmer <dpalmer@college.harvard.edu>
2013-04-21 17:24:18 -0400

    tweaked scheduler.

--- include/thread.h
+++ include/thread.h
@@ -56,9 +56,6 @@ struct vnode;
 /* Macro to test if two addresses are on the same kernel stack */
 #define SAME_STACK(p1, p2)     (((p1) & STACK_MASK) == ((p2) & STACK_MASK))
 
-/* Max priority of a thread */
-#define PRIORITY_MAX 7
-
 /* States a thread can be in. */
 typedef enum {
 	S_RUN,		/* running */
