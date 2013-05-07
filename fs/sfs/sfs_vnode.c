1cfbfc100fb5653812dcbee398b84f829c2c46a6
David Palmer <dpalmer@college.harvard.edu>
2013-05-07 16:02:11 -0400

    Fixed bug in dotruncate

--- fs/sfs/sfs_vnode.c
+++ fs/sfs/sfs_vnode.c
@@ -2103,7 +2103,7 @@ sfs_dotruncate(struct vnode *v, off_t len, struct transaction *txn)
 					else if(id_modified)
 					{
 						/* The indirect block has been modified */
-                        txn_attach(txn, tidbuf);
+                        txn_attach(txn, idbuf);
 						buffer_mark_dirty(idbuf);
 						if(indir != 1)
 						{
@@ -2144,7 +2144,7 @@ sfs_dotruncate(struct vnode *v, off_t len, struct transaction *txn)
 					if(indir == 2)
 					{
 						inodeptr->sfi_dindirect = 0;
-                        txn_attach(txn, tidbuf);
+                        txn_attach(txn, sv->sv_buf);
 						buffer_mark_dirty(sv->sv_buf);
 					}
 					if(indir == 3)
@@ -2156,7 +2156,7 @@ sfs_dotruncate(struct vnode *v, off_t len, struct transaction *txn)
 				else if(did_modified)
 				{
 					/* The double indirect block has been modified */
-                    txn_attach(txn, tidbuf);
+                    txn_attach(txn, didbuf);
 					buffer_mark_dirty(didbuf);
 					if(indir == 3)
 					{
